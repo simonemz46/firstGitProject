345f5f5aae3150525e34007d4ad43d2ee48502ea
simonemz46 <morazendrinisimone@gmail.com>
2021-11-17 11:09:10 +0100

    Added coment line 4 from rep2

--- letsgo.c
+++ letsgo.c
@@ -1,9 +1,14 @@
+/*added comment from repository 1*/
 #include<stdio.h>
-int main()
+
+int main() //another comment from repository 2
 {
 printf("Git,let's go!!\n");
 printf("Added line1!");
 printf("working on a new feature 1...\n");
+
 return 0;
 }
+/*added comment from repository 2*/
+
 
